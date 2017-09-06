1d8ac010433d6339a5d5642ced5acbf2bc2613c3
Steven Nguyen <stevenan1296@gmail.com>
2017-09-06 14:34:50 -0400

    added bus reminder

--- txt_reminders.py
+++ txt_reminders.py
@@ -2,6 +2,9 @@ from twilio.rest import Client
 from datetime import datetime
 import keys
 import time
+import xml.etree.ElementTree as ET
+import urllib.request
+
 
 def get_current_time():
     full_time = str(datetime.now().time())
@@ -9,6 +12,50 @@ def get_current_time():
     formatted_time = full_time[0][:-3]
     return formatted_time
 
+def remind_at_time(sender, receiver):
+    information = input('enter a message: ')
+    alert_time = input('enter a time: ')
+    print(alert_time)
+    
+    while True:
+        current_time = get_current_time()
+        print(current_time)
+        if alert_time == current_time:
+            message = twilioCli.messages.create(body=information, from_=sender, to=receiver)
+            break
+        else:
+            time.sleep(30)
+
+def remind_bus_time(sender, receiver):
+    while True:
+        try:
+            url = ('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=chapel-hill&r=CM&s=camerobe_e')
+            decoded_route = urllib.request.urlopen(url).read().decode('utf-8')
+        except:
+            print('error')
+        
+        bus = 'bus.xml'
+        file = open(bus,'w')
+        
+        file.write(decoded_route)
+        
+        file.close()
+        
+        tree = ET.parse(bus)
+        root = tree.getroot()
+        
+        for p in root.findall('predictions'):
+            for directions in p.findall('direction'):
+                for predictions in directions.findall('prediction'):
+                    time_left = predictions.attrib['minutes']
+                    information = time_left + " minutes until CM arrives to home."
+                    print(time_left)
+                    if time_left <= '40':
+                        message = twilioCli.messages.create(body=information, from_=sender, to=receiver)
+                        break
+        
+        time.sleep(120)
+                    
 accountSID = keys.accountSID
 authToken = keys.authToken
 
@@ -17,15 +64,5 @@ twilioCli = Client(accountSID, authToken)
 myTwilioNumber = keys.myTwilioNumber
 myNumber = keys.myNumber
 
-information = input('enter a message: ')
-alert_time = input('enter a time: ')
-print(alert_time)
-
-while True:
-    current_time = get_current_time()
-    print(current_time)
-    if alert_time == current_time:
-        message = twilioCli.messages.create(body=information, from_=myTwilioNumber, to=myNumber)
-        break
-    else:
-        time.sleep(30)
+remind_bus_time(myTwilioNumber, myNumber)
+print('done')
